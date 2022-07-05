33b12a2c4a030cbec8176656356d67b8c6c76a85
zabdiel1 <jonathanalbayero203@hotmail.com>
2022-07-04 19:08:05 -0600

    manejo de plantillas en jsf

---
+++ 04-JavaEE/JSF/BolsaTrabajoV9/src/main/java/beans/helper/ColoniaHelper.java
@@ -0,0 +1,66 @@
+package beans.helper;
+
+import beans.model.Colonia;
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.model.SelectItem;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class ColoniaHelper {
+
+    public List<Colonia> getColonias() {
+        List<Colonia> colonias = new ArrayList<>();
+
+        int coloniaId = 1;
+        Colonia colonia = new Colonia(coloniaId++, "Napoles", 3810);
+        colonias.add(colonia);
+
+        colonia = new Colonia(coloniaId++, "Polanco", 11530);
+        colonias.add(colonia);
+
+        colonia = new Colonia(coloniaId++, "Valle Centro", 3100);
+        colonias.add(colonia);
+
+        return colonias;
+    }
+
+    public int getColoniaIdPorNombre(String nombreColonia) {
+        int coloniaId = 0;
+        List<Colonia> colonias = this.getColonias();
+
+        for (Colonia colonia : colonias) {
+            if (colonia.getNombrecolonia().equals(nombreColonia)) {
+                coloniaId = colonia.getColoniaId();
+                break;
+            }
+        }
+        return coloniaId;
+    }
+
+    public int getColoniaIdPorCodigoPostal(int codigoPostal) {
+        int coloniaId = 0;
+        List<Colonia> colonias = this.getColonias();
+
+        for (Colonia colonia : colonias) {
+            if (colonia.getCodigoPostal() == codigoPostal) {
+                coloniaId = colonia.getColoniaId();
+                break;
+            }
+        }
+        return coloniaId;
+    }
+
+    public List<SelectItem> getSelectItems() {
+        List<SelectItem> selectItems = new ArrayList<>();
+        List<Colonia> colonias = this.getColonias();
+
+        for (Colonia colonia : colonias) {
+            SelectItem selectItem = new SelectItem(colonia.getColoniaId(), colonia.getNombrecolonia());
+            selectItems.add(selectItem);
+        }
+        return selectItems;
+    }
+}
