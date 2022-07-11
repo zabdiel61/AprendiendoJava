53b63b8ca1367dbb8dd1900f48ea322a7b1f5598
zabdiel1 <jonathanalbayero203@hotmail.com>
2022-07-11 08:20:29 -0600

    Implementacion de usuario en proyecto sga

---
+++ 04-JavaEE/EJB/Leccion02/hola/src/main/java/com/zd/sga/hola/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.zd.sga.hola;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
