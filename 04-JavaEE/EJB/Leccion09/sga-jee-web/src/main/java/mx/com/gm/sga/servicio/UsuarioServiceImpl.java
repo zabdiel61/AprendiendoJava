419f99e7a018e711c9769d2698787ac0e0cc2880
zabdiel1 <jonathanalbayero203@hotmail.com>
2022-07-12 22:18:04 -0600

    Implementacion de JSF Primefaces EJB y JPA

---
+++ 04-JavaEE/EJB/Leccion09/sga-jee-web/src/main/java/mx/com/gm/sga/servicio/UsuarioServiceImpl.java
@@ -0,0 +1,40 @@
+package mx.com.gm.sga.servicio;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import mx.com.gm.sga.datos.UsuarioDao;
+import mx.com.gm.sga.domain.Usuario;
+
+@Stateless
+public class UsuarioServiceImpl implements UsuarioServiceRemote, UsuarioService{
+
+    @Inject
+    private UsuarioDao usuarioDao;
+    
+    @Override
+    public List<Usuario> listarUsuarios() {
+       return usuarioDao.findAllUsuarios();
+    }
+
+    @Override
+    public Usuario encontrarUsuarioPorId(Usuario usuario) {
+        return usuarioDao.findUsuarioById(usuario);
+    }
+
+    @Override
+    public void registrarUsuario(Usuario usuario) {
+        usuarioDao.insertUsuario(usuario);
+    }
+
+    @Override
+    public void modificarUsuario(Usuario usuario) {
+        usuarioDao.updateUsuario(usuario);
+    }
+
+    @Override
+    public void eliminarUsuario(Usuario usuario) {
+        usuarioDao.deleteUsuario(usuario);
+    }
+    
+}
